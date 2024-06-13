9ca8d45c306d68c7ca041c80d9b24b1a9983b50a
Atik Faysal <66104543+atik666@users.noreply.github.com>
2024-06-13 20:26:15 +0000

    6/13 2

---
+++ src/utils/data_loaders/noise_loader.py
@@ -0,0 +1,41 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms # type: ignore
+import numpy as np
+import os
+from signal_loader import CustomSignalDataset
+import random
+
+def noise_generation(signal, snr_db):
+    signal_power = np.mean(signal**2) # calculate signal power
+    #calculate noise power
+    snr_linear = 10**(snr_db/10)
+    noise_power = signal_power/snr_linear
+    noise = np.sqrt(noise_power) * np.random.randn(len(signal))
+    return noise
+
+class NoiseLoader(Dataset):
+    def __init__(self, signal_dir, snr_db=None, transform=None):
+        self.signal_dir = signal_dir
+        self.transform = transform
+        self.signal_files = os.listdir(signal_dir)
+        self.snr_db = snr_db
+
+    def __len__(self):
+        return len(self.signal_files)
+    
+    def __getitem__(self, index):
+        signal_path = os.path.join(self.signal_dir, self.signal_files[index])
+        signal = np.load(signal_path)
+
+        if self.transform:
+            signal = self.transform(signal)
+
+        if not self.snr_db:
+            snr_db = random.uniform(-10, 10)
+        
+        noise = noise_generation(signal, snr_db)
+
+        noisy_signal = signal + noise
+
+        return noisy_signal, noise
